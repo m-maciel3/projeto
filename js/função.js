d2f77e029ab7928d86d98b4398143b4010931dba
maria m <maria.maciel0308@hotmail.com>
2022-10-28 11:15:31 -0400

    alteração

--- js/função.js
+++ js/função.js
@@ -1,18 +1,4 @@
-      function mostrarOcultarSenha(){    
-        var mostrarSenha = document.querySelector("#mostrarSenha").style.display;
-        
-        if(mostrarSenha == "block"){
-        document.querySelector("#ocultarSenha").style.display="block";
-        document.querySelector("#mostrarSenha").style.display="none";
-        document.querySelector("#novaSenha").type="text";
-        }
-            else 
-            {
-                document.querySelector("#ocultarSenha").style.display="none";
-                document.querySelector("#mostrarSenha").style.display="block";
-                document.querySelector("#novaSenha").type="password";     
-            }
-    }
+      
     function mostrarOcultarNovaSenha(){    
         var mostrarSenha = document.querySelector("#mostrarNovaSenha").style.display;
         
@@ -26,5 +12,19 @@
                 document.querySelector("#ocultarNovaSenha").style.display="none";
                 document.querySelector("#mostrarNovaSenha").style.display="block";
                 document.querySelector("#confirmarNovaSenha").type="password";     
+            }
+    }function mostrarOcultarSenha(){    
+        var mostrarSenha = document.querySelector("#mostrarSenha").style.display;
+        
+        if(mostrarSenha == "block"){
+        document.querySelector("#ocultarSenha").style.display="block";
+        document.querySelector("#mostrarSenha").style.display="none";
+        document.querySelector("#novaSenha").type="text";
+        }
+            else 
+            {
+                document.querySelector("#ocultarSenha").style.display="none";
+                document.querySelector("#mostrarSenha").style.display="block";
+                document.querySelector("#novaSenha").type="password";     
             }
     }
