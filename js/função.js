54c0f84a16166e1f0d65b1a00f5acafd0b7f0f9e
m-maciel3 <113068841+m-maciel3@users.noreply.github.com>
2022-11-01 01:48:45 -0400

    novos arquivos

--- js/função.js
+++ js/função.js
@@ -1,30 +1,30 @@
-      
-    function mostrarOcultarNovaSenha(){    
-        var mostrarSenha = document.querySelector("#mostrarNovaSenha").style.display;
-        
-        if(mostrarSenha == "block"){
-        document.querySelector("#ocultarNovaSenha").style.display="block";
-        document.querySelector("#mostrarNovaSenha").style.display="none";
-        document.querySelector("#confirmarNovaSenha").type="text";
-        }
-            else 
-            {
-                document.querySelector("#ocultarNovaSenha").style.display="none";
-                document.querySelector("#mostrarNovaSenha").style.display="block";
-                document.querySelector("#confirmarNovaSenha").type="password";     
-            }
-    }function mostrarOcultarSenha(){    
+    function mostrarOcultarSenha(){    
         var mostrarSenha = document.querySelector("#mostrarSenha").style.display;
         
         if(mostrarSenha == "block"){
-        document.querySelector("#ocultarSenha").style.display="block";
         document.querySelector("#mostrarSenha").style.display="none";
+        document.querySelector("#ocultarSenha").style.display="block";
         document.querySelector("#novaSenha").type="text";
         }
             else 
             {
-                document.querySelector("#ocultarSenha").style.display="none";
                 document.querySelector("#mostrarSenha").style.display="block";
+                document.querySelector("#ocultarSenha").style.display="none";
                 document.querySelector("#novaSenha").type="password";     
             }
-    }
+
+    } function mostrarOcultarNovaSenha(){    
+        var mostrarSenha = document.querySelector("#mostrarNovaSenha").style.display;
+        
+        if(mostrarSenha == "block"){
+        document.querySelector("#mostrarNovaSenha").style.display="none";
+        document.querySelector("#ocultarNovaSenha").style.display="block";
+        document.querySelector("#confirmarNovaSenha").type="text";
+        }
+            else 
+            {
+                document.querySelector("#mostrarNovaSenha").style.display="block";
+                document.querySelector("#ocultarNovaSenha").style.display="none";
+                document.querySelector("#confirmarNovaSenha").type="password";     
+            }
+        }
